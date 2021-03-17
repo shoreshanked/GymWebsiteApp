431db7d56e59e5003e6945cacd5e81ea892acd94
shoreshanked <60507967+shoreshanked@users.noreply.github.com>
2021-03-17 22:28:57 +0000

    Added a dictionary of Exercise on training block
    Added a dictionary of Exercise on training block to be used with myCharts

--- GymWebsite/GymWebsite/wwwroot/js/myChart.js
+++ GymWebsite/GymWebsite/wwwroot/js/myChart.js
@@ -1,10 +1,10 @@
-﻿var ctx = document.getElementByID('myChart').getContext('2d');
+﻿var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12'],
         datasets: [{
-            label: 'Will Bench Max (kg)',
+            label: 'Bench',
             data: [100, 120, 135, 130, 135, 140, 145, 150, 150, 145, 160, 165],
             backgroundColor: [
                 'rgba(0, 0, 0, 0.0)',
@@ -12,10 +12,10 @@ var myChart = new Chart(ctx, {
             borderColor: [
                 'rgba(255, 99, 132, 1)',
             ],
-            borderWIDth: 3
+            borderWidth: 3
             },
             {
-                label: 'Jonny Bench Max (kg)',
+                label: 'Squat',
                 data: [110, 130, 135, 140, 145, 100, 135, 120, 150, 160, 170, 190],
                 backgroundColor: [
                     'rgba(0, 0, 0, 0.0)',
@@ -23,10 +23,10 @@ var myChart = new Chart(ctx, {
                 borderColor: [
                     'rgba(54, 162, 235, 1)',
                 ],
-                borderWIDth: 3
+                borderWidth: 3
             },
             {
-                label: 'Alex Bench Max (kg)',
+                label: 'Deadlift',
                 data: [90, 140, 145, 150, 135, 80, 115, 150, 160, 170, 200, 180],
                 backgroundColor: [
                     'rgba(0, 0, 0, 0.0)',
@@ -34,7 +34,7 @@ var myChart = new Chart(ctx, {
                 borderColor: [
                     'rgba(255, 206, 86, 1)',
                 ],
-                borderWIDth: 3
+                borderWidth: 3
             }]
     },
     options: {
